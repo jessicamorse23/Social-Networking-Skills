bf1e3e0eed45dde677758e750ccaae91c1d20c75
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-29 11:57:15 -0400

    added thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router;
+const {
+  getThoughts, 
+  getOneThought,
+  userCreateThought,
+  updateOneThought,
+  deleteThought,
+  addOneReaction,
+  deleteOneReaction,
+
+} = require("../../controllers/thoughtController");
+
+// for testing = api/thoughts
+router.route("/").get(getThoughts).post(userCreateThought);
+
+// for testing = api/thoughts/:id
+router
+.route("/id")
+.get(getOneThought)
+.put(updateOneThought)
+.delete(deleteThought)
+
+// for testing api/thoughts/:thoughtId/reactions
+router.route("/thoughtId/reactions").post(addOneReaction);
+
+//  for testing api/thoughts:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/reactionId").delete(deleteOneReaction);
+
+module.exports = router;
+
+
+

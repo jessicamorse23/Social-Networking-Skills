b9a10cefd10788532b27b9f88051f288e2a528e2
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-29 12:11:01 -0400

    added index.js to Routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// code from NU Module 18.26 index.js Routes
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  return res.send('Wrong route!');
+});
+
+module.exports = router;
